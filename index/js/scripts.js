78ec8a3315000da713ef0d477dc4228e3710afdc
zikriazlan <124416593+zikriazlan@users.noreply.github.com>
2023-02-04 11:52:22 +0800

    addnewfile

---
+++ index/js/scripts.js
@@ -0,0 +1,170 @@
+/* Description: Custom JS file */
+
+/* Navigation*/
+// Collapse the navbar by adding the top-nav-collapse class
+window.onscroll = function () {
+	scrollFunction();
+	scrollFunctionBTT(); // back to top button
+};
+
+window.onload = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 30) {
+		document.getElementById("navbarExample").classList.add("top-nav-collapse");
+	} else if ( document.documentElement.scrollTop < 30 ) {
+		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
+	}
+}
+
+// Navbar on mobile
+let elements = document.querySelectorAll(".nav-link:not(.dropdown-toggle)");
+
+for (let i = 0; i < elements.length; i++) {
+	elements[i].addEventListener("click", () => {
+		document.querySelector(".offcanvas-collapse").classList.toggle("open");
+	});
+}
+
+document.querySelector(".navbar-toggler").addEventListener("click", () => {
+  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
+});
+
+// Hover on desktop
+function toggleDropdown(e) {
+	const _d = e.target.closest(".dropdown");
+	let _m = document.querySelector(".dropdown-menu", _d);
+
+	setTimeout(
+		function () {
+		const shouldOpen = _d.matches(":hover");
+		_m.classList.toggle("show", shouldOpen);
+		_d.classList.toggle("show", shouldOpen);
+
+		_d.setAttribute("aria-expanded", shouldOpen);
+		},
+		e.type === "mouseleave" ? 300 : 0
+	);
+}
+
+// On hover
+const dropdownCheck = document.querySelector('.dropdown');
+
+if (dropdownCheck !== null) { 
+	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
+	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
+
+	// On click
+	document.querySelector(".dropdown").addEventListener("click", (e) => {
+		const _d = e.target.closest(".dropdown");
+		let _m = document.querySelector(".dropdown-menu", _d);
+		if (_d.classList.contains("show")) {
+			_m.classList.remove("show");
+			_d.classList.remove("show");
+		} else {
+			_m.classList.add("show");
+			_d.classList.add("show");
+		}
+	});
+}
+  
+
+/* Card Slider - Swiper */
+var cardSlider = new Swiper('.card-slider', {
+	autoplay: {
+		delay: 4000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+});
+
+/* Back To Top Button */
+// Get the button
+myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunctionBTT() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		myButton.style.display = "block";
+	} else {
+		myButton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // for Safari
+	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+}
+
+ /* Details Lightbox - Magnific Popup */
+    $('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+    });
+	
+	
+    /* Filter - Isotope */
+    var $grid = $('.grid').isotope({
+        // options
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
+    
+    // filter items on button click
+    $('.filters-button-group').on( 'click', 'a', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+    
+    // change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'a', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+        });	
+    });
+	
+ $( document ).ready(function() {
+	/* Rotating Text - Morphtext */
+	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 2000,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+    });
+ });
+ 
+  $( document ).ready(function() {
+	/* Rotating Text - Morphtext */
+	$("#js-rotating1").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 2000,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+    });
+ });
